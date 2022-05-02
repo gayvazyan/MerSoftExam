e59c2adf4b53a77aa219ae0a2013c968ab4b0341
garegin.ayvazyan <garegin.ayvazyan@outlook.com>
2022-05-02 11:08:08 +0400

    InitialCommite

---
+++ ShopAppMerSoftExam.Core/Entities/ShopAppDbContect.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopAppMerSoftExam.Core.Entities
+{
+    public class ShopAppDbContect : DbContext
+    {
+        public ShopAppDbContect(DbContextOptions<ShopAppDbContect> options)
+            : base(options)
+        {
+        }
+
+       
+        //public DbSet<Product> ProductDb { get; set; }
+
+    }
+}
---
+++ ShopAppMerSoftExam.Core/ServicesInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopAppMerSoftExam.Core.Entities;
+
+namespace ShopAppMerSoftExam.Core
+{
+    public static partial class ServicesInitializer
+    {
+        public static void ConfigureDbContext(IConfiguration configuration, IServiceCollection services)
+        {
+
+
+            services.AddDbContextPool<ShopAppDbContect>(
+              options => options.UseSqlServer(configuration.GetConnectionString("AppDbConnection")));
+
+
+
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddTransient(typeof(IRepositories<>), typeof(Repositories<>));
+            //services.AddTransient<IProductRepasitory, ProductRepasitory>();
+            
+        }
+    }
+}
